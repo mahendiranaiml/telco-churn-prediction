8b12db9269b878f6f99c20c01a51151c36b267b2
Mahendiran <mahendiranaiml@gmail.com>
2026-04-22 12:05:25 +0000

    MLOps

--- tests/test_ingestion.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-from src.ingestion import CSVDatasetIngestor
-
-
-def test_validator_accepts_required_columns():
-    data = pd.read_csv("data/raw/WA_Fn-UseC_-Telco-Customer-Churn.csv")
-    CSVDatasetIngestor("data/raw/WA_Fn-UseC_-Telco-Customer-Churn.csv").validate_dataframe(data)
